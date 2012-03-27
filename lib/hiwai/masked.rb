7cfa58e77debab8d47b27c1c1514ba98453d9397
hibariya <celluloid.key@gmail.com>
2012-03-27 20:08:11 +0900

    use included

--- lib/hiwai/masked.rb
+++ lib/hiwai/masked.rb
@@ -1,11 +1,15 @@
 module Hiwai
   module Masked
+    def self.included(klass)
+      klass.class_eval do
+        alias_method :to_s, :masked_string
+        alias_method :inspect, :masked_string
+      end
+    end
+
     def masked_string
       '<masked>'
     end
-
-    alias_method :to_s, :masked_string
-    alias_method :inspect, :masked_string
   end
 
   class MaskedString < String
